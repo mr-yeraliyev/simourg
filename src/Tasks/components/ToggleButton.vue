954e77865357a543fcdb4209a21a701667a8bf5a
mr_yeral1yev <daryn.yeraliyev@sdu.edu.kz>
2024-06-20 14:39:17 +0500

    convert string to style object

--- src/Tasks/components/ToggleButton.vue
+++ src/Tasks/components/ToggleButton.vue
@@ -1,25 +1,12 @@
 <template>
   <span>
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect x="0.5" y="0.5" width="19" height="19" rx="1.5" stroke="#CED1D9" />
-      <rect
-        :x="x"
-        :y="y"
-        :width="width"
-        :height="height"
-        fill="#CED1D9"
-        class="state-rect"
-      />
-    </svg>
+    <span class="container">
+      <div class="outer-rect">
+        <div :style="innerRectStyle" class="inner-rect" />
+      </div>
+    </span>
   </span>
 </template>
-
 <script setup lang="ts">
 import { computed } from 'vue'
 
@@ -30,15 +17,39 @@ const props = defineProps({
   },
 })
 
-const x = computed(() => (props.state === 1 ? 14 : 0.5))
+const x = computed(() => (props.state === 1 ? 14 : 0))
 const y = computed(() => (props.state === 3 ? 14 : 0))
 const width = computed(() => (props.state === 2 || props.state === 3 ? 20 : 6))
 const height = computed(() => (props.state === 2 || props.state === 3 ? 6 : 20))
+
+const innerRectStyle = computed(() => ({
+  left: `${x.value}px`,
+  top: `${y.value}px`,
+  width: `${width.value}px`,
+  height: `${height.value}px`,
+  backgroundColor: '#CED1D9',
+}))
 </script>
 
-<style scoped>
-.state-rect {
-  transition-property: x, height, y, width;
+<style lang="scss" scoped>
+.container {
+  position: relative;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+}
+
+.outer-rect {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #ced1d9;
+  border-radius: 1.5px;
+}
+
+.inner-rect {
+  position: absolute;
+  transition-property: height width top left;
   transition-duration: 0.4s;
   transition-timing-function: ease;
 }
--- src/Tasks/pages/SecondVersionPage.vue
+++ src/Tasks/pages/SecondVersionPage.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container">
-    <span class="vector_1" :style="states[state].vector_1" />
-    <span class="vector_2" :style="states[state].vector_2" />
-    <span class="vector_3" :style="states[state].vector_3" />
-    <div class="input-group" :style="states[state].inputGroup">
-      <label :style="states[state].inputLabel" class="input-label">Label</label>
-      <span :style="states[state].inputField" class="input-field" />
-    </div>
+    <div :style="currentState.inputGroup" class="input-group" />
+    <div :style="currentState.inputLabel" class="input-label">Label</div>
+    <div :style="currentState.inputField" class="input-field" />
+
     <div class="values">
-      <div :style="states[state].values_1" class="values_1">
+      <div :style="currentState.values_1" class="values_1">
+        <div :style="currentState.vector_1" class="vector" />
         <input type="number" />
       </div>
-      <div :style="states[state].values_2" class="values_2">
+      <div :style="currentState.values_2" class="values_2">
+        <div :style="currentState.vector_2" class="vector" />
         <input type="number" />
       </div>
-      <div :style="states[state].values_3" class="values_3">
+      <div :style="currentState.values_3" class="values_3">
+        <div :style="currentState.vector_3" class="vector" />
         <input type="number" />
       </div>
       <ToggleButton :state="state" @click="changeState" class="toggle-button" />
@@ -23,185 +23,113 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, computed, StyleValue } from 'vue'
 import ToggleButton from '../components/ToggleButton.vue'
 
 interface State {
-  inputGroup: string
-  inputLabel: string
-  inputField: string
-  values_1: string
-  values_2: string
-  values_3: string
-  vector_1: string
-  vector_2: string
-  vector_3: string
+  inputGroup: StyleValue
+  inputLabel: StyleValue
+  inputField: StyleValue
+  values_1: StyleValue
+  values_2: StyleValue
+  values_3: StyleValue
+  vector_1: StyleValue
+  vector_2: StyleValue
+  vector_3: StyleValue
 }
 
 const state = ref(0)
 const changeState = () => {
-  state.value === 3 ? (state.value = 0) : state.value++
+  state.value = (state.value + 1) % 4
 }
-// You can put this into static values
-const states: State[] = [
-  {
-    inputGroup: 'height: 40px;',
-    inputLabel: 'width: 28.4%; transform: translate(17%, 27%);',
-    inputField: 'width: 51.1%; transform: translate(64%, 27%);',
-    values_1: 'transform: translate(0, 0);',
-    values_2: 'transform: translate(130%, 0);',
-    values_3: 'transform: translate(260%, 0);',
-    vector_1: 'height: 47px;',
-    vector_2: 'height: 47px;',
-    vector_3: 'height: 38px;',
+
+const states: Record<string, State> = {
+  state_1: {
+    inputGroup: { height: '40px' },
+    inputLabel: { width: '28.4%', transform: 'translate(17%, 27%)' },
+    inputField: { width: '51.1%', transform: 'translate(64%, 27%)' },
+    values_1: { transform: 'translate(0, 0)' },
+    values_2: { transform: 'translate(130%, 0)' },
+    values_3: { transform: 'translate(260%, 0)' },
+    vector_1: { height: '56px' },
+    vector_2: { height: '56px' },
+    vector_3: { height: '47px' },
   },
-  {
-    inputGroup: 'height: 40px;',
-    inputLabel: 'width: 28.4%; transform: translate(205%, 27%);',
-    inputField: 'width: 51.1%; transform: translate(17%, 27%);',
-    values_1: 'transform: translate(260%, 0);',
-    values_2: 'transform: translate(130%, 0);',
-    values_3: 'transform: translate(0, 0);',
-    vector_1: 'height: 38px;',
-    vector_2: 'height: 47px;',
-    vector_3: 'height: 47px;',
+  state_2: {
+    inputGroup: { height: '40px' },
+    inputLabel: { width: '28.4%', transform: 'translate(205%, 27%)' },
+    inputField: { width: '51.1%', transform: 'translate(17%, 27%)' },
+    values_1: { transform: 'translate(260%, 0)' },
+    values_2: { transform: 'translate(130%, 0)' },
+    values_3: { transform: 'translate(0, 0)' },
+    vector_1: { height: '56px' },
+    vector_2: { height: '56px' },
+    vector_3: { height: '47px' },
   },
-  {
-    inputGroup: 'height: 62px;',
-    inputLabel: 'width: 79.5%; transform: translate(10%, 27%);',
-    inputField: 'width: 65%; transform: translate(34%, 120%);',
-    values_1: 'transform: translate(0, 0);',
-    values_2: 'transform: translate(130%, 0);',
-    values_3: 'transform: translate(260%, 0);',
-    vector_1: 'height: 43px;',
-    vector_2: 'height: 18px;',
-    vector_3: 'height: 13px;',
+  state_3: {
+    inputGroup: { height: '62px' },
+    inputLabel: { width: '79.5%', transform: 'translate(10%, 27%)' },
+    inputField: { width: '65%', transform: 'translate(34.5%, 120%)' },
+    values_1: { transform: 'translate(0, 0)' },
+    values_2: { transform: 'translate(130%, 0)' },
+    values_3: { transform: 'translate(260%, 0)' },
+    vector_1: { height: '56px' },
+    vector_2: { height: '32px' },
+    vector_3: { height: '26px' },
   },
-  {
-    inputGroup: 'height: 62px;',
-    inputLabel: 'width: 65%; transform: translate(34%, 120%);',
-    inputField: 'width: 79.5%; transform: translate(10%, 27%);',
-    values_1: 'transform: translate(130%, 0);',
-    values_2: 'transform: translate(0, 0);',
-    values_3: 'transform: translate(260%, 0);',
-    vector_1: 'height: 43px;',
-    vector_2: 'height: 18px;',
-    vector_3: 'height: 13px;',
+  state_4: {
+    inputGroup: { height: '62px' },
+    inputLabel: { width: '65%', transform: 'translate(34.5%, 120%)' },
+    inputField: { width: '79.5%', transform: 'translate(10%, 27%)' },
+    values_1: { transform: 'translate(130%, 0)' },
+    values_2: { transform: 'translate(0, 0)' },
+    values_3: { transform: 'translate(260%, 0)' },
+    vector_1: { height: '32px' },
+    vector_2: { height: '56px' },
+    vector_3: { height: '26px' },
   },
-]
+}
+
+const currentState = computed(() => states[`state_${state.value + 1}`])
 </script>
 
 <style lang="scss" scoped>
 .container {
   position: relative;
   width: 100%;
-  background-color: red;
+}
 
-  .vector_1 {
-    position: absolute;
-    bottom: -89px;
-    left: 55px;
-    width: 1px;
-    background-color: #ced1d9;
-
-    z-index: 100;
-    &::before {
-      content: '';
-      position: absolute;
-      top: -2px;
-      left: -1.3px;
-      width: 4px;
-      height: 4px;
-      background-color: #ced1d9;
-      border-radius: 50%;
-    }
-
-    transition: height 0.4s ease;
-  }
-  .vector_2 {
-    position: absolute;
-    bottom: -89px;
-    left: 106px;
-    width: 1px;
-    background-color: #ced1d9;
-
-    z-index: 100;
-    &::before {
-      content: '';
-      position: absolute;
-      top: -2px;
-      left: -1.3px;
-      width: 4px;
-      height: 4px;
-      background-color: #ced1d9;
-      border-radius: 50%;
-    }
-
-    transition: height 0.4s ease;
-  }
-  .vector_3 {
-    position: absolute;
-    bottom: -89px;
-    left: 158px;
-    width: 1px;
-    background-color: #ced1d9;
-
-    z-index: 100;
-    &::before {
-      content: '';
-      position: absolute;
-      top: -2px;
-      left: -1.3px;
-      width: 4px;
-      height: 4px;
-      background-color: #ced1d9;
-      border-radius: 50%;
-    }
-
-    transition: height 0.4s ease;
-  }
+.input-group,
+.input-label,
+.input-field {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition-property: width height transform;
+  transition-duration: 0.4s;
+  transition-timing-function: ease;
 }
 
 .input-group {
   background-color: #f7f7f7;
   border: 1px solid #ced1d9;
-  position: absolute;
-  top: 0;
-  left: 0;
   width: 100%;
   border-radius: 4px;
-  transform: translate(20%, 20%);
-
-  transition: height 0.4s ease;
 }
 
 .input-label {
   color: #969696;
   display: flex;
-
   height: 24px;
   justify-content: center;
   align-items: center;
   border: 1px dashed #ced1d9;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transition-property: transform, width;
-  transition-duration: 0.4s;
-  transition-timing-function: ease;
 }
 
 .input-field {
   border: 1px solid #ced1d9;
-
   height: 24px;
   background-color: #ffffff;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transition-property: transform, width;
-  transition-duration: 0.4s;
-  transition-timing-function: ease;
 }
 
 .values {
@@ -209,51 +137,43 @@ const states: State[] = [
   position: absolute;
   top: 0;
   left: 0;
-  transform: translate(20%, 90px);
-  &_1 {
+  transform: translate(0, 90px);
+}
+
+.values_1,
+.values_2,
+.values_3 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 22.7%;
+  height: 30px;
+  transition: transform 0.4s ease;
+}
+
+.vector {
+  position: absolute;
+  bottom: 31px;
+  left: 50%;
+  width: 1px;
+  background-color: #ced1d9;
+  z-index: 100;
+
+  &::before {
+    content: '';
     position: absolute;
-    top: 0;
-    left: 0;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 22.7%;
-    height: 30px;
-
-    transition-property: transform;
-    transition-duration: 0.4s;
-    transition-timing-function: ease;
+    top: -2px;
+    left: -1.3px;
+    width: 4px;
+    height: 4px;
+    background-color: #ced1d9;
+    border-radius: 50%;
   }
-  &_2 {
-    position: absolute;
-    top: 0;
-    left: 0;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 22.7%;
-    height: 30px;
-
-    transition-property: transform;
-    transition-duration: 0.4s;
-    transition-timing-function: ease;
-  }
-  &_3 {
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 22.7%;
-    height: 30px;
-
-    transition-property: transform, width;
-    transition-duration: 0.4s;
-    transition-timing-function: ease;
-  }
+  transition: height 0.4s ease;
 }
 
 .toggle-button {
