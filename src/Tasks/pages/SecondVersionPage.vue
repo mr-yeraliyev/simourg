038606f46e98c9886ea732e5b21c0c11481ffc67
mr_yeral1yev <daryn.yeraliyev@sdu.edu.kz>
2024-06-21 11:45:41 +0500

    convert styles to BEM

--- src/Tasks/pages/SecondVersionPage.vue
+++ src/Tasks/pages/SecondVersionPage.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="container">
-    <div :style="currentState.inputGroup" class="input-group" />
-    <div :style="currentState.inputLabel" class="input-label">Label</div>
-    <div :style="currentState.inputField" class="input-field" />
+    <div :style="currentState.inputGroup" class="container__input-group" />
+    <div :style="currentState.inputLabel" class="container__input-label">
+      Label
+    </div>
+    <div :style="currentState.inputField" class="container__input-field" />
 
-    <div class="values">
-      <div :style="currentState.values_1" class="values_1">
-        <div :style="currentState.vector_1" class="vector" />
+    <div class="container__values">
+      <div
+        :style="currentState.values_1"
+        class="container__values-item container__values-item--1"
+      >
+        <div
+          :style="currentState.vector_1"
+          class="container__vector container__vector--1"
+        />
         <input type="number" :disabled="state === 4" />
       </div>
-      <div :style="currentState.values_2" class="values_2">
-        <div :style="currentState.vector_2" class="vector" />
+      <div
+        :style="currentState.values_2"
+        class="container__values-item container__values-item--2"
+      >
+        <div
+          :style="currentState.vector_2"
+          class="container__vector container__vector--2"
+        />
         <input type="number" />
       </div>
-      <div :style="currentState.values_3" class="values_3">
-        <div :style="currentState.vector_3" class="vector" />
+      <div
+        :style="currentState.values_3"
+        class="container__values-item container__values-item--3"
+      >
+        <div
+          :style="currentState.vector_3"
+          class="container__vector container__vector--3"
+        />
         <input type="number" />
       </div>
-      <ToggleButton :state="state" @click="changeState" class="toggle-button" />
+      <ToggleButton
+        :state="state"
+        @click="changeState"
+        class="container__toggle-button"
+      />
     </div>
   </div>
 </template>
@@ -113,89 +137,100 @@ const currentState = computed(() => states[`state_${state.value + 1}`])
 .container {
   position: relative;
   width: 100%;
-}
-
-.input-group,
-.input-label,
-.input-field {
-  position: absolute;
-  top: 0;
-  left: 0;
-  transition-property: width height transform;
-  transition-duration: 0.4s;
-  transition-timing-function: ease;
-}
-
-.input-group {
-  background-color: #f7f7f7;
-  border: 1px solid #ced1d9;
-  width: 100%;
-  border-radius: 4px;
-}
-
-.input-label {
-  color: #969696;
-  display: flex;
-  height: 24px;
-  justify-content: center;
-  align-items: center;
-  border: 1px dashed #ced1d9;
-}
-
-.input-field {
-  border: 1px solid #ced1d9;
-  height: 24px;
-  background-color: #ffffff;
-}
-
-.values {
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transform: translate(0, 90px);
-}
-
-.values_1,
-.values_2,
-.values_3 {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 22.7%;
-  height: 30px;
-  transition: transform 0.4s ease;
-}
-
-.vector {
-  position: absolute;
-  bottom: 31px;
-  left: 50%;
-  width: 1px;
-  background-color: #ced1d9;
-  z-index: 100;
-
-  &::before {
-    content: '';
+  &__toggle-button {
     position: absolute;
-    top: -2px;
-    left: -1.3px;
-    width: 4px;
-    height: 4px;
-    background-color: #ced1d9;
-    border-radius: 50%;
+    bottom: -30px;
+    right: 0;
   }
 
-  transition: height 0.4s ease;
-}
+  &__input-group,
+  &__input-label,
+  &__input-field {
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition-property: width height transform;
+    transition-duration: 0.4s;
+    transition-timing-function: ease;
+  }
+  &__input-group {
+    background-color: #f7f7f7;
+    border: 1px solid #ced1d9;
+    width: 100%;
+    border-radius: 4px;
+  }
+  &__input-label {
+    color: #969696;
+    display: flex;
+    height: 24px;
+    justify-content: center;
+    align-items: center;
+    border: 1px dashed #ced1d9;
+  }
+  &__input-field {
+    border: 1px solid #ced1d9;
+    height: 24px;
+    background-color: #ffffff;
+  }
+  &__values {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translate(0, 90px);
+  }
+  &__values-item {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 22.7%;
+    height: 30px;
+    transition: transform 0.4s ease;
 
-.toggle-button {
-  position: absolute;
-  bottom: -30px;
-  right: 0;
+    &--1 {
+      transform: translate(0, 0);
+    }
+    &--2 {
+      transform: translate(130%, 0);
+    }
+    &--3 {
+      transform: translate(260%, 0);
+    }
+  }
+  &__vector {
+    position: absolute;
+    bottom: 31px;
+    left: 50%;
+    width: 1px;
+    background-color: #ced1d9;
+    z-index: 100;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: -2px;
+      left: -1.3px;
+      width: 4px;
+      height: 4px;
+      background-color: #ced1d9;
+      border-radius: 50%;
+    }
+
+    transition: height 0.4s ease;
+
+    &--1 {
+      height: 56px;
+    }
+    &--2 {
+      height: 56px;
+    }
+    &--3 {
+      height: 47px;
+    }
+  }
 }
 
 input {
